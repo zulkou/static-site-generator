5046d75bde2cd40a2f073b5c5c776add0f940f3c
zulkou <73563319+zulkou@users.noreply.github.com>
2025-01-21 19:27:51 +0700

    add generate page function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from split_block import extract_title, markdown_to_html_node
 
 def copy_static(src, dst):
     if os.path.exists(dst):
@@ -15,6 +16,27 @@ def copy_static(src, dst):
         else:
             print(f"created \"{dst_path}\" directory")
             copy_static(src_path, dst_path)
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from \"{from_path}\" to \"{dest_path}\" using \"{template_path}\"")
+    with open(from_path) as src, open(template_path) as tmplt:
+        source = src.read()
+        template = tmplt.read()
         
+        content = markdown_to_html_node(source)
+        print(f"Type of content: {type(content)}")  # Add this debug line
+        html_content = content.to_html()
+
+        title = extract_title(source)
+
+        # Replace both placeholders in the template
+        final_html = template.replace("{{ Title }}", title)
+        final_html = final_html.replace("{{ Content }}", html_content)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+
+    with open(dest_path, "w") as dest:
+        dest.write(final_html)
 
 copy_static("static", "public")
+generate_page("content/index.md", "template.html", "public/index.html")
